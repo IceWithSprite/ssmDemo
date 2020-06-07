056e395c24403e8d51c4e50df6aebc69247239dc
IceWithSprite <871194445@qq.com>
2020-06-07 21:14:23 +0800

    ssmDemo项目

---
+++ SsmDemo/src/main/java/com/aisino/controller/AdminController.java
@@ -0,0 +1,50 @@
+package com.aisino.controller;
+
+
+import com.aisino.annotation.SysLog;
+
+import com.aisino.pojo.TbAdmin;
+
+import com.aisino.service.AdminService;
+
+import org.apache.commons.lang.StringUtils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+@Controller
+@RequestMapping("sys")
+public class AdminController {
+
+	@RequestMapping("/adminList")
+	public String adminList() {
+		return "page/admin/adminList";
+	}
+	
+	@RequestMapping("/menuList")
+	public String menuList() {
+		return "page/admin/menuList";
+	}
+	
+	@RequestMapping("/personalData")
+	public String personalData(HttpServletRequest req) {
+//		TbAdmin admin=(TbAdmin) req.getSession().getAttribute("admin");
+
+
+		return "page/admin/personalData";
+	}
+	
+
+}
